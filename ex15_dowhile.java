28e013583c7635eb3be8a58df8043303465203ee
Huy Tran <baohuy82.th@gmail.com>
2023-10-12 00:50:45 -0700

    commit

---
+++ ex15_dowhile.java
@@ -0,0 +1,11 @@
+public class ex15_dowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        int sum = 0;
+        do {
+            sum += a;
+            a++;
+        } while (a<=5);
+        System.out.println("sum = " + sum);
+    }
+}
