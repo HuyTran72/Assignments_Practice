641ab8f817e2f6e52c747908c9eb5d668b7ed100
Huy Tran <baohuy82.th@gmail.com>
2023-10-12 00:07:30 -0700

    commit

---
+++ switch_case/example.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input an integer: ");
+        int a = sc.nextInt();
+
+        int div = a%2;
+        switch (div) {
+            case 0: 
+                System.out.println(a + " is an even");
+                break;
+            default:
+                System.out.println(a + " is an odd");
+                break;
+        }
+    }
+}
