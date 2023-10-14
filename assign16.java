6e92ef5d9fc8119af11efd94413169e5922e74c2
Huy Tran <baohuy82.th@gmail.com>
2023-10-14 14:33:22 -0700

    commit

---
+++ assign16.java
@@ -0,0 +1,13 @@
+public class assign16 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int fac = 1;
+
+        for(int i = 1; i <= 10; i++) {
+            fac *= i;
+            sum += fac;
+        }
+        System.out.println(sum);
+
+    }
+}
