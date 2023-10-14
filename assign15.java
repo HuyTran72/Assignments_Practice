2d0230ea0b4eade529fca6e03422ca4083942d65
Huy Tran <baohuy82.th@gmail.com>
2023-10-14 14:24:13 -0700

    commit

---
+++ assign15.java
@@ -0,0 +1,11 @@
+public class assign15 {
+    public static void main(String[] args) {
+        int n = 50;
+
+        for (int i = 10; i <= n; i++) {
+            if (i%3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
