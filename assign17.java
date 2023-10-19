6ecf7c0f910f143e81627ba02bd3df6589ffee18
Huy Tran <baohuy82.th@gmail.com>
2023-10-18 23:23:15 -0700

    commit

--- assign17.java
+++ assign17.java
@@ -1,5 +1,11 @@
 public class assign17 {
     public static void main(String[] args) {
-        
+
+        for (int i = 1; i <= 1000; i++) {
+            int sum = 0;
+            for (int j = 1; j <= i/2; j++) {
+
+            }
+            }
+        }
     }
-}
