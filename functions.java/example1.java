6c35b7441a8d76ebd2994c1fb1c832b4e8410046
Huy Tran <baohuy82.th@gmail.com>
2023-10-20 00:02:30 -0700

    commit

--- functions.java/example1.java
+++ functions.java/example1.java
@@ -4,8 +4,8 @@ public class example1 {
         String result2 = Solvept2(1, 2, 1);
         String result3 = Solvept2(1, 2, -3);
         System.out.println(result1);
-        System.out.println(result1);
-        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
     }
 
     public static String Solvept2(double a, double b, double c) {
@@ -22,11 +22,11 @@ public class example1 {
             if (delta <0) {
                 return ("No solution");
             } else if (delta == 0) {
-                double x = -b/ 2*a;
+                double x = -b/ (2*a);
                 return ("Only 1 solution x = " + x);
             } else {
-                double x1 = (-b + Math.sqrt(delta))/ 2*a;
-                double x2 = (-b - Math.sqrt(delta))/ 2*a;
+                double x1 = (-b + Math.sqrt(delta))/ (2*a);
+                double x2 = (-b - Math.sqrt(delta))/ (2*a);
                 return ("Has 2 solutions x1 = " + x1 + " x2 = " + x2);
             }
         }
