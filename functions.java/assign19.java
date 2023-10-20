78426e10ce523da8ded12b525ee62a33f332e962
Huy Tran <baohuy82.th@gmail.com>
2023-10-20 01:11:09 -0700

    commit

--- functions.java/assign19.java
+++ functions.java/assign19.java
@@ -3,27 +3,29 @@ import java.util.Scanner;
 public class assign19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input a = ");
+        System.out.print("Please input a = ");
         double a = sc.nextDouble();
-        System.out.println("Please input b = ");
+        System.out.print("Please input b = ");
         double b = sc.nextDouble();
         System.out.print("Please input the operation (+ or - or * or /) op: ");
-        char op = sc.next().charAt(0);
+        String op = sc.nextLine();
+        double rel = Operation1(a, b, op);
+        System.out.println(rel);
     }
 
-    public static Double Operation1(double a, double b, char op) {
+    public static Double Operation1(double a, double b, String op) {
+        double result = 0;
         try {
-            if (op.equals(+)) {
+            if (op.equals("+")) {
             return a+b;
-        } else if (op.equals(-)) {
+        } else if (op.equals("-")) {
             return a-b;
-        }else if (op.equals(*)) {
+        }else if (op.equals("*")) {
             return a*b;
-        } else (op.equals(/)) {
+        } else if (op.equals("/")) {
             while (b == 0) {
                 System.out.println("Please input b again, b is not equal to 0");
                 System.out.println("Input b = ");
-                double b = sc.nextDouble;
             }
             return a/b;
         }
@@ -31,6 +33,6 @@ public class assign19 {
             System.out.println("The denominator is not equal to 0");
             e.printStackTrace();
         }
-        
+        return result;
     }
 }
