cc23a3ba481762487a9f95225e570acdf435d4ac
Huy Tran <baohuy82.th@gmail.com>
2023-10-19 22:48:10 -0700

    commit

---
+++ try_cat.java/lesson20.java
@@ -0,0 +1,10 @@
+public class lesson20 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = a / b;
+
+        System.out.println("c = " + c );
+        System.out.println("Print code");
+    }
+}
