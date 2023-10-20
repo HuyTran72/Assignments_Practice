3a6a84aab17adad4943b9aebaf3d01de00137006
Huy Tran <baohuy82.th@gmail.com>
2023-10-19 23:36:32 -0700

    commit

---
+++ functions.java/lesson21.java
@@ -0,0 +1,13 @@
+public class lesson21 {
+    public static void main(String[] args) {
+        int result = sum(1, 2, 3);
+        System.out.println(result);
+    }
+
+    public static int sum(int x, int y, int z) {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        return x+y+z;
+    }
+}
