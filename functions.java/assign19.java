4baf2e24dca6eff9cb268f1ba542de92e7cc655a
Huy Tran <baohuy82.th@gmail.com>
2023-10-20 20:49:28 -0700

    commit

--- functions.java/assign19.java
+++ functions.java/assign19.java
@@ -8,15 +8,13 @@ public class assign19 {
         System.out.print("Please input b = ");
         double b = sc.nextDouble();
         System.out.print("Please input the operation (+ or - or * or /) op: ");
-        String op = sc.nextLine();
+        String op = new Scanner(System.in).nextLine();
         double rel = Operation1(a, b, op);
         System.out.println(rel);
     }
 
     public static Double Operation1(double a, double b, String op) {
-        double result = 0;
-        try {
-            if (op.equals("+")) {
+        if (op.equals("+")) {
             return a+b;
         } else if (op.equals("-")) {
             return a-b;
@@ -25,14 +23,12 @@ public class assign19 {
         } else if (op.equals("/")) {
             while (b == 0) {
                 System.out.println("Please input b again, b is not equal to 0");
-                System.out.println("Input b = ");
+                System.out.print("Input b = ");
+                b = new Scanner(System.in).nextDouble();
+
             }
             return a/b;
         }
-        } catch (ArithmeticException e) {
-            System.out.println("The denominator is not equal to 0");
-            e.printStackTrace();
-        }
-        return result;
+        return a;
     }
 }
