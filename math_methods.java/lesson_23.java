d2ba615a7cf573b22e554e60b86bd948d1be6550
Huy Tran <baohuy82.th@gmail.com>
2023-10-21 13:11:29 -0700

    commit

--- math_methods.java/lesson_23.java
+++ math_methods.java/lesson_23.java
@@ -1,3 +1,12 @@
+import javax.sound.midi.Soundbank;
+
 public class lesson_23 {
-    
+    public static void main(String[] args) {
+        System.out.println("Pi = " + Math.PI);
+        int a = -8;
+        System.out.println(Math.abs(a));
+        System.out.println(Math.max(4, 7));
+        System.out.println(Math.min(1, 5));
+        System.out.println(Math.sqrt(4));
+    }
 }
