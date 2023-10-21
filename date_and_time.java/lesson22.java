305d8e4b9f52d36221007bb3b39c3807fbcea5b6
Huy Tran <baohuy82.th@gmail.com>
2023-10-20 21:13:34 -0700

    commit

--- date_and_time.java/lesson22.java
+++ date_and_time.java/lesson22.java
@@ -12,5 +12,14 @@ public class lesson22 {
         System.out.println(month+1);
         System.out.println(day);
 
+        cal.set(Calendar.YEAR, 2023);
+        cal.set(Calendar.MONTH, 07);
+        cal.set(Calendar.DAY_OF_MONTH, 02);
+
+        int year1 = cal.get(Calendar.YEAR);
+        int month1 = cal.get(Calendar.MONTH);
+        int day1 = cal.get(Calendar.DAY_OF_MONTH);
+
+        System.out.println("Born in " + month1 + "/" + day1 + "/" + year1);
     }
 }
