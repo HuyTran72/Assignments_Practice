aabc4aa81667a5b2b6971aaf56f2fba9de4adbcb
Huy Tran <baohuy82.th@gmail.com>
2023-10-20 21:09:04 -0700

    commit

---
+++ date_and_time.java/lesson22.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+
+public class lesson22 {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal);
+
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        System.out.println(year);
+        System.out.println(month+1);
+        System.out.println(day);
+
+    }
+}
