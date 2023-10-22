bed315038e30bad3e3c29a7b6ea420b33d203aa0
Huy Tran <baohuy82.th@gmail.com>
2023-10-22 10:46:37 -0700

    commit

--- character/lesson25.java
+++ character/lesson25.java
@@ -1,10 +1,24 @@
-package character;
+import java.util.Scanner;
 
 public class lesson25 {
     public static void main(String[] args) {
         char ch = 'a';
         char ch2 = 66;
-        System.out.println("gg");
+        System.out.println(ch2);
+
+        char ch3;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input an character: ");
+        String s = sc.nextLine();
+        char ch4 = s.charAt(0);
+        System.out.println("Char: " + ch4);
+        
+        //compare
+        System.out.println(Character.compare('b', 'b'));
+        System.out.println(Character.compare('a', 'b'));
+        System.out.println(Character.compare('A', 'a'));
+
     }
 }
     
