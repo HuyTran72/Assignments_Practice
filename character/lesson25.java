5e3c44a73a5d580ec6c4ffe0497e8bbf4719ac1d
Huy Tran <baohuy82.th@gmail.com>
2023-10-21 23:25:48 -0700

    commit

--- character/lesson25.java
+++ character/lesson25.java
@@ -1,5 +1,10 @@
 package character;
 
 public class lesson25 {
-    
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch2 = 66;
+        System.out.println("gg");
+    }
 }
+    
