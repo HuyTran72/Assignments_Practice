b5fee409885413520db3224e1dedb41b9af24cca
Huy Tran <baohuy82.th@gmail.com>
2023-10-22 10:58:05 -0700

    commit

--- character/lesson25.java
+++ character/lesson25.java
@@ -8,17 +8,25 @@ public class lesson25 {
 
         char ch3;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input an character: ");
-        String s = sc.nextLine();
-        char ch4 = s.charAt(0);
-        System.out.println("Char: " + ch4);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Input an character: ");
+        // String s = sc.nextLine();
+        // char ch4 = s.charAt(0);
+        // System.out.println("Char: " + ch4);
         
         //compare
         System.out.println(Character.compare('b', 'b'));
         System.out.println(Character.compare('a', 'b'));
         System.out.println(Character.compare('A', 'a'));
 
+        // char method
+        char ch5 = '1';
+        char ch6 = 'a';
+        char ch7 = 'A';
+        char ch8 = '1';
+
+        System.out.println(Character.isDigit(ch5));
+        System.out.println(Character.isUpperCase(ch8));
     }
 }
     
