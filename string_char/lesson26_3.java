8519e00f05efe74fb34f1558bcb251282872ed13
Huy Tran <baohuy82.th@gmail.com>
2023-10-23 11:07:27 -0700

    commit

---
+++ string_char/lesson26_3.java
@@ -0,0 +1,9 @@
+public class lesson26_3 {
+    public static void main(String[] args) {
+        String s25 = "abc123456";
+        String s26 = "abc123456";
+
+        int x = s25.compareTo(s26);
+        System.out.println("x = " + x);
+    }
+}
