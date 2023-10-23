12d553d2e5eefcce47e8f78e0b851557fafb2d6d
Huy Tran <baohuy82.th@gmail.com>
2023-10-23 11:19:49 -0700

    commit

--- string_char/lesson26_3.java
+++ string_char/lesson26_3.java
@@ -8,10 +8,22 @@ public class lesson26_3 {
 
         //split array
         String s38 = "Hello, world";
-        String[] arr = s38.split(",");
+        String[] arr = s38.split(", ");
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
+        }
+
+        String s39 = "I am programer";
+        String s40 = s39.toLowerCase();
+        System.out.println(s40);
+        String s41 = s39.toUpperCase();
+        System.out.println(s41);
+
+        String s42 = "abcdefg1244";
+        char[] arr2 = s42.toCharArray(); 
+        for (int i =0; i< arr2.length; i++) {
+            System.out.println(arr2[i]);
         }
     }
 }
