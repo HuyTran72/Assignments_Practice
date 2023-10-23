c8ae305672247366bbc25792a5a928231af9a943
Huy Tran <baohuy82.th@gmail.com>
2023-10-23 11:14:17 -0700

    commit

--- string_char/lesson26_3.java
+++ string_char/lesson26_3.java
@@ -1,9 +1,17 @@
 public class lesson26_3 {
     public static void main(String[] args) {
-        String s25 = "abc123456";
+        String s25 = "abc1234567";
         String s26 = "abc123456";
 
         int x = s25.compareTo(s26);
         System.out.println("x = " + x);
+
+        //split array
+        String s38 = "Hello, world";
+        String[] arr = s38.split(",");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
