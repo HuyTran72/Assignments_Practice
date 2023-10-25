b73e5ec56896e2df7b4b9f18ff5e69901ba0e919
Huy Tran <baohuy82.th@gmail.com>
2023-10-24 22:42:00 -0700

    commit

--- string_char/assgign19.java
+++ string_char/assgign19.java
@@ -2,7 +2,30 @@ import java.util.Scanner;
 
 public class assgign19 {
     public static void main(String[] args) {
-        System.out.println("Input an string: ");
+        System.out.print("Input an string: ");
         String str = new Scanner(System.in).nextLine();
+        int count_Num = 0;
+        int count_low = 0;
+        int count_Upp = 0;
+        int count_Spa = 0;
+
+        System.out.println("The length of the string : " + str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(4);
+            if(Character.isLowerCase(c)) {
+                count_low++;
+            } else if (Character.isUpperCase(c)) {
+                count_Upp++;
+            } else if (Character.isDigit(c)) {
+                count_Num++;
+            } else if (Character.isWhitespace(c)){
+                count_Spa++;
+            }
+        }
+        System.out.println("Number of lowercase = " + count_low);
+        System.out.println("Number of uppercase = " + count_Upp);
+        System.out.println("Number of num = " + count_Num);
+        System.out.println("Number of Space = " + count_Spa);
+
     }
 }
