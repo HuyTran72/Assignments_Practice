ecd42ec42786f9a79df5001669c5b76171181f41
Huy Tran <baohuy82.th@gmail.com>
2023-10-24 23:29:12 -0700

    commit

--- string_char/assign_19.java
+++ string_char/assign_19.java
@@ -3,20 +3,22 @@ public class assign_19 {
         String str1 = "English = 78 Science = 83 Math = 68 History = 65";
         int sum = 0;
         int count = 0;
+        
         String[] parts = str1.split(" ");
 
-        for (String pt1 : parts) {
-            System.out.println(pt1);
-        }
-        try {
-            int num = Integer.parseInt(str1);
-            sum += num;
-            count++;
-        } catch (NumberFormatException e){
+        for(String pt : parts) {
+            System.out.println(pt);
+            try {
+                int num = Integer.parseInt(pt);
+                sum += num;
+                count++;
+            }catch (NumberFormatException e){
 
+            }
         }
-        double mean = sum/ count;
+        System.out.println();
         System.out.println(sum);
+        double mean = sum / count;
         System.out.println(mean);
     }
 }
