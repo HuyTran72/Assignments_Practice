cb786715d4c4acbea37873839947e4a72de5e418
Huy Tran <baohuy82.th@gmail.com>
2023-10-26 23:31:15 -0700

    commit

--- string_char/assign_26.java
+++ string_char/assign_26.java
@@ -4,6 +4,7 @@ public class assign_26 {
     public static void main(String[] args) {
         String input = "     Ga LaI LAp     TRinH     ";
         System.out.println(standardized(input));
+
     }
 
     public static String standardized(String input) {
@@ -16,5 +17,13 @@ public class assign_26 {
             words[i] = firstChar + words[i].substring(1);
         }
         return String.join(" ", words);
+    }
+
+    public static String checkinput(String input) {
+        String noSpace = input.trim().replace("\\s", "").toLowerCase();
+        String upperCas = noSpace.substring(0,1).toUpperCase() + noSpace.substring(1);
+        System.out.println(noSpace);
+        System.out.println(upperCas);
+        return null;
     }
 }
