fdfe009dd868d2db599abb51440959ff9c52783b
Huy Tran <baohuy82.th@gmail.com>
2023-10-29 23:28:39 -0700

    commit

--- arrayList/lesson_28.java
+++ arrayList/lesson_28.java
@@ -15,7 +15,25 @@ public class lesson_28 {
         lst4.add(7);
         lst4.add(8);
         lst4.add(11);
+        lst4.add(1, 99);
         System.out.println("Lst4 is: " + lst4);
+
+        //size
+        System.out.println("The number of elements is: " + lst4.size());
+
+        //get(int index)
+        System.out.println(lst4.get(3));
+
+        //remove
+        lst4.remove(2);
+        System.out.println(lst4);
+
+        ArrayList<Integer> lst5 = new ArrayList<>(List.of(1,2,3,4,5,6));
+        System.out.println(lst5);
+        lst5.remove(Integer.valueOf(3));
+        System.out.println(lst5);
+
+
 
     }
 }
