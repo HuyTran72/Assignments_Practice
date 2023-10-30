d6a2675c37735c7d06c0267dca9163f7c31043ad
Huy Tran <baohuy82.th@gmail.com>
2023-10-29 22:37:10 -0700

    commit

---
+++ arrayList/lesson_28.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lesson_28 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lst = new ArrayList<>();
+        ArrayList<Integer> lst2 = new ArrayList<>(5);
+        ArrayList<Integer> lst3 = new ArrayList<>(List.of(1,2,3,4,5,6));
+
+        System.out.println(lst);
+        System.out.println(lst2);
+        System.out.println(lst3);
+
+        ArrayList<Integer> lst4 = new ArrayList<>();
+        lst4.add(7);
+        lst4.add(8);
+        lst4.add(11);
+        System.out.println("Lst4 is: " + lst4);
+
+    }
+}
