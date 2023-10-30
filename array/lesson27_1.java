6c7a67799c86ef17edbe585d47dd4a4abb535d24
Huy Tran <baohuy82.th@gmail.com>
2023-10-29 18:45:49 -0700

    commit

--- array/lesson27_1.java
+++ array/lesson27_1.java
@@ -1,5 +1,12 @@
-package array;
-
 public class lesson27_1 {
-    
+    public static void main(String[] args) {
+        String[] mangStr = new String[12];
+        int[] mangNguyen2 = new int[7];
+
+        String[] mangStr2 = new String[] {"LOL", "PUBG"};
+        int[] mangNguyen3 = new int[] {1,2,4};
+
+        System.out.println(mangStr2[1]);
+        System.out.println(mangNguyen3[2]);
+    }
 }
