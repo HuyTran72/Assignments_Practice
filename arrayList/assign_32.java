117d17cf46a231ac9f087b62f0f7ef4bd6817ace
Huy Tran <baohuy82.th@gmail.com>
2023-11-01 00:33:48 -0700

    commit

---
+++ arrayList/assign_32.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class assign_32 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lst = new ArrayList<>(List.of(1,9,3,14,5,27,8));
+        Collections.sort(lst);
+        System.out.println(lst);
+
+        
+    }
+}
