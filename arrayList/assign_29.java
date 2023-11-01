72803952f2f9ac5704a6ecea58a652e9bafb80ec
Huy Tran <baohuy82.th@gmail.com>
2023-10-31 23:31:45 -0700

    commit

--- arrayList/assign_29.java
+++ arrayList/assign_29.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 public class assign_29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input number of elements n = ");
+        System.out.print("Input number of elements n = ");
         int n = sc.nextInt();
 
-        ArrayList<Integer> lst = new ArrayList<>(n);
+        ArrayList<Integer> lst = new ArrayList<>();
         for(int i =0; i<n; i++) {
-            System.out.println(lst.indexOf(i));
+            System.out.print("Input the element " + (i+1) + ": ");
             int num = sc.nextInt();
+            lst.add(num);
         }
-        System.out.println(lst);
+        System.out.println("The list is: " + lst);
+
+        System.out.println("----------");
+        
+        ArrayList<Integer> lstSquare = new ArrayList<>();
+        System.out.print("Input number of elements n = ");
+        n = sc.nextInt();
+        for(int i = 0; i<n ; i++) {
+            System.out.print("Input the element " + (i+1) + ": ");
+            int numSq = sc.nextInt();
+            lstSquare.add(numSq*numSq);
+        }
+        System.out.println("The list square of elements is: " + lstSquare);
     }
 }
