7ec6171b82241ad4b258a130ddfa6df59c3e9b55
Huy Tran <baohuy82.th@gmail.com>
2023-11-01 00:54:02 -0700

    commit

--- arrayList/assign_32.java
+++ arrayList/assign_32.java
@@ -6,9 +6,19 @@ import java.util.List;
 public class assign_32 {
     public static void main(String[] args) {
         ArrayList<Integer> lst = new ArrayList<>(List.of(1,9,3,14,5,27,8));
-        Collections.sort(lst);
-        System.out.println(lst);
+        ArrayList<Integer> copy = new ArrayList<>(lst);
+        Collections.sort(copy);
+        System.out.println(copy);
 
+        String index = "";
+        int numLar2 = copy.get(copy.size() - 2);
+        int numSmal2 = copy.get(1);
         
-    }
+        System.out.println(numLar2);
+        System.out.println(numSmal2);
+
+        //Check index
+        System.out.println("Index of numLar2: " + lst.indexOf(numLar2));
+        System.out.println("Index of numSmal2: " + lst.indexOf(numSmal2));
+    }  
 }
